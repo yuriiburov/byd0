638d0f30895839c09e3cddc2bbc54b813667ad59
yuriiburov <burovyuriireact@gmail.com>
2021-12-10 19:33:42 +0200

    init

--- js-full/lesson28/task1/index.js
+++ js-full/lesson28/task1/index.js
@@ -1,6 +1,5 @@
-const calc = (initialValue) => {
-  let result = initialValue;
-
+export const calc = (initValue) => {
+  let result = initValue;
   const calculator = {
     add(value) {
       result += value;
@@ -12,23 +11,22 @@ const calc = (initialValue) => {
       return this;
     },
 
+    subtract(value) {
+      result -= value;
+      return this;
+    },
+
     div(value) {
       result /= value;
       return this;
     },
 
-    subtract(value) {
-      result -= value;
-      return this;
-    },
     result() {
       return result;
     },
   };
-
   return calculator;
 };
+const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
 
-// const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
-
-// console.log(result); // -3
+console.log(result);
